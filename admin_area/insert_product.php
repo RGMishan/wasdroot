0857111a39f65d73c1b67edd0116b11b5ff39a3d
rgmishan <mishanregmi@gmail.com>
2020-03-06 12:38:37 +0530

    inserting product;

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -44,9 +44,9 @@ include ("includes/db.php");
 				</div><!--  ..panel bodyclosed -->
 				
 				<div class="panel-body"> <!-- //panel heading started -->
-					<form class="form-horizontal" method="post" action="multipart/form-data">
+					<form class="form-horizontal" method="post" enctype="multipart/form-data">
 						<div class="form-group">
-							<label class="col-md-3 control-label">Product Title</label>
+							<label class="col-md-3 control-label">Game Name</label>
 							<input type="text" name="product_title" class="form-control" required="" >
 						</div>
 						<div class="form-group">
@@ -91,7 +91,7 @@ include ("includes/db.php");
 						</div>
 						<div class="form-group">
 							<label class="col-md-3 control-label">Product Image 2</label>
-							<input type="file" name="product_img3" class="form-control" required="" >
+							<input type="file" name="product_img2" class="form-control" required="" >
 						</div>
 						<div class="form-group">
 							<label class="col-md-3 control-label">Product Image 3</label>
@@ -103,11 +103,17 @@ include ("includes/db.php");
 						</div>
 						<div class="form-group">
 							<label class="col-md-3 control-label">Product Keywoard</label>
-							<input type="text" name="product_keywoards" class="form-control" required="" >
+							<input type="text" name="product_keywords" class="form-control" required="" >
 						</div>
 						<div class="form-group">
 							<label class="col-md-3 control-label">Product Description</label>
 							<textarea name ="product_desc" class="form-control" rows="6" cols="9"></textarea> <!-- //USING THE TINY HERE -->
+
+						<div class="form-group">
+							<input type="submit" name="submit" value="Insert Product" class="btn btn-primary form-control">
+						</div>
+
+
 						</div>
 					</form>
 
@@ -131,7 +137,7 @@ include ("includes/db.php");
 
 
 <?php
-if (isset($POST['submit'])) {
+if (isset($_POST['submit'])) {
 	$product_title=$_POST['product_title'];
 	$product_cat=$_POST['product_cat'];
 	$product_cat=$_POST['cat'];
@@ -139,10 +145,29 @@ if (isset($POST['submit'])) {
 	$product_desc=$_POST['product_desc'];
 	$product_keywords=$_POST['product_keywords'];
 
-	$product_img1=$_FILE['product_img1'];
-	$product_img2=$_FILE['product_img2'];
-	$product_img3=$_FILE['product_img3'];
+	$product_img1=$_FILES['product_img1']['name'];
+	$product_img2=$_FILES['product_img2']['name'];
+	$product_img3=$_FILES['product_img3']['name'];
+
+	$temp_name1=$_FILES['product_img1']['tmp_name'];
+	$temp_name2=$_FILES['product_img2']['tmp_name'];
+	$temp_name3=$_FILES['product_img3']['tmp_name'];
+	
+	move_uploaded_file($temp_name1, "product_images/$product_img1");
+	move_uploaded_file($temp_name2, "product_images/$product_img2");
+	move_uploaded_file($temp_name3, "product_images/$product_img3");
+
+	$inset_product = "INSERT INTO products (p_cat_id, cat_id, date, product_title, product_img1, product_img2, product_img3, product_price, product_desc, product_keywords) VALUES ( '$product_cat', '$product_cat', NOW() , '$product_title', '$product_img1', '$product_img2', '$product_img3', '$product_price' , '$product_desc', '$product_keywords' )";
+
+	$run_product = mysqli_query($con, $inset_product);
+
+	if ($run_product) {
+		echo "<script>alert('Product Inserted Successfuly')</script>";
+		echo "<script>window.open('insert_product.php')</script>";
+		echo "Inserted Testing Caseinner";
+	echo "test2";
+	}
 	
 }
-
+// START FROM 68th vid
 ?>
